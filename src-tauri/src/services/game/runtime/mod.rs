0644a1bedabd059edba21346b15fe83b1f8cc21c
16steyy <pavlovkamil73@gmail.com>
2026-05-25 19:52:27 +0300

    fix: fix call

--- src-tauri/src/services/game/runtime/mod.rs
+++ src-tauri/src/services/game/runtime/mod.rs
@@ -7,6 +7,9 @@ mod session;
 
 pub use env::load_project_env_for_runtime;
 
+#[cfg(target_os = "linux")]
+pub(crate) use env::apply_linux_display_env;
+
 pub(crate) use downloads::*;
 pub(crate) use jvm::*;
 pub(crate) use launch_prep::*;
