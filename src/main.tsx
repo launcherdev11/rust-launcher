4679a3c470e9a769b7b2c399c4e721c34a4694b6
16steyy <@.com>
2026-05-31 12:00:08 +0300

    feat: console window

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ConsoleApp } from "./ConsoleApp";
+import { isGameConsoleWindowView } from "./lib/gameConsoleWindow";
 import "./index.css";
+
+const Root = isGameConsoleWindowView() ? ConsoleApp : App;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
